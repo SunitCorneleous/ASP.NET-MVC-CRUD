27ac95ca28a1bdba8fc6cafdbf8c3e2a8a5e9164
Sunit Corneleous <sunit.corneleous25@gmail.com>
2023-09-25 11:42:12 +0600

    js updated

--- CRUDMVC/Scripts/addEmployee.js
+++ CRUDMVC/Scripts/addEmployee.js
@@ -1,52 +1,6 @@
 ﻿$(document).ready(function () {
-    $("#openModal").click(
-        function () {
+    console.log()
 
-            $('.modal-title').text("Add Employee");
-            $('#addEmployeeButton').text("Add");
-
-            $("#ename").val("");
-            $("#eemail").val("");
-            $("#eaddress").val("");
-            $("#eposition").val("");
-
-        }
-    )
-
-    $("#addEmployeeButton").click(function () {
-        var addOrUpdateActionButton = $("#addEmployeeButton").text();
-
-        // Add Employee
-        if (addOrUpdateActionButton === "Add") {
-            let Name = $("#ename").val();
-            let Email = $("#eemail").val();
-            let Address = $("#eaddress").val();
-            let Position = $("#eposition").val();
-
-            let employee = {
-                Name,
-                Email,
-                Address,
-                Position
-            }
-
-            if (Name && Email && Address && Position) {
-                console.log("add api call");
-
-                $.post("https://localhost:44395/Employee/CreateEmployee",
-                    {
-                        emodel: employee
-                    },
-                    function (data, status) {
-                        console.log(data);
-
-                        location.reload(true);
-                    });
-            }
-        }
-
-        
-    });
-
+   
 
 });
--- CRUDMVC/Scripts/mydatatablejquery.js
+++ CRUDMVC/Scripts/mydatatablejquery.js
@@ -3,7 +3,7 @@
         var employeeId;
         var employeeData;
 
-        $("#employeeTable").DataTable({
+        var table = $("#employeeTable").DataTable({
             "ajax": {
                 "url": "/Employee/GetEmployeeData",
                 "type": "GET",
@@ -85,7 +85,9 @@
                 function (data, status) {
                     console.log(data);
 
-                    location.reload(true);
+                    $('#deleteModal').modal('hide')
+
+                    table.ajax.reload();
                 });
         });
 
@@ -118,11 +120,65 @@
                         function (data, status) {
                             console.log(data);
 
-                            location.reload(true);
+                            $('#myModal').modal('hide');
+
+                            table.ajax.reload();
                         });
                 }
 
             }
         });
+
+        $("#openModal").click(
+            function () {
+
+                $('.modal-title').text("Add Employee");
+                $('#addEmployeeButton').text("Add");
+
+                $("#ename").val("");
+                $("#eemail").val("");
+                $("#eaddress").val("");
+                $("#eposition").val("");
+
+            }
+        )
+
+        $("#addEmployeeButton").click(function () {
+            var addOrUpdateActionButton = $("#addEmployeeButton").text();
+
+            // Add Employee
+            if (addOrUpdateActionButton === "Add") {
+                let Name = $("#ename").val();
+                let Email = $("#eemail").val();
+                let Address = $("#eaddress").val();
+                let Position = $("#eposition").val();
+
+                let employee = {
+                    Name,
+                    Email,
+                    Address,
+                    Position
+                }
+
+                if (Name && Email && Address && Position) {
+                    console.log("add api call");
+
+                    $.post("https://localhost:44395/Employee/CreateEmployee",
+                        {
+                            emodel: employee
+                        },
+                        function (data, status) {
+                            console.log(data);
+
+                            $('#myModal').modal('hide');
+
+                            table.ajax.reload();
+                        });
+                }
+            }
+
+
+        });
+
     }
 )
